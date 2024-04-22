008e577af19bf59e025a6f5ca245c457f27255af
Thomas Olinger <olingerthomas95@gmail.com>
2024-04-22 00:02:04 -0400

    fixed workouts

--- server/routes/record.js
+++ server/routes/record.js
@@ -8,22 +8,13 @@ recordRoutes.route("/record/add").post(async (req, response) => {
     const db = await dbo.getDb();
     const { userID, workoutName, exercises } = req.body;
 
-    
-      const sessionId = req.cookies.sessionId; // assuming you're using cookies for session management
-      const user = await db.users.findOne({ sessionId: sessionId });
-  
-      if (user) {
-          req.user = user;
-          next();
-      
-  
     // Create a new workout document
     const newWorkout = {
       date: new Date(),
       name: workoutName,
       userID: userID,
       favorited: false,
-      completed: false,
+      completed: Array(exercises.length).fill(false),
       exercises: exercises // Array of exercises
     };
 
@@ -31,11 +22,7 @@ recordRoutes.route("/record/add").post(async (req, response) => {
     if (workoutName !== "") {
       const result = await db.collection("workouts").insertOne(newWorkout);
       response.status(201).json({ message: "Workout added successfully", workoutId: result.insertedId });
-    }}
-
-    else {
-      res.status(401).json({ message: 'Unauthorized' });
-  }
+    }
   } catch (error) {
     console.error("Error inserting new workout:", error);
     response.status(500).json({ error: "Internal server error" });
